74a9f5a62798dda81bb3dc68da2c178cb16836fc
yutaaa06 <kd1382074@st.kobedenshi.ac.jp>
2024-04-23 15:23:21 +0900

    現時点での全てのプロジェクトをコミット

---
+++ L01Camera01/Src/Application/main.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+//============================================================
+// アプリケーションクラス
+//	APP.～ でどこからでもアクセス可能
+//============================================================
+class Application
+{
+// メンバ
+public:
+
+	// アプリケーション実行
+	void Execute();
+
+	// アプリケーション終了
+	void End() { m_endFlag = true; }
+
+	HWND GetWindowHandle() const { return m_window.GetWndHandle(); }
+	int GetMouseWheelValue() const { return m_window.GetMouseWheelVal(); }
+
+	int GetNowFPS() const { return m_fpsController.m_nowfps; }
+	int GetMaxFPS() const { return m_fpsController.m_maxFps; }
+
+private:
+
+	void KdBeginUpdate();
+	void PreUpdate();
+	void Update();
+	void PostUpdate();
+	void KdPostUpdate();
+
+	void KdBeginDraw(bool usePostProcess = true);
+	void PreDraw();
+	void Draw();
+	void PostDraw();
+	void DrawSprite();
+	void KdPostDraw();
+
+	// アプリケーション初期化
+	bool Init(int w, int h);
+
+	// アプリケーション解放
+	void Release();
+
+	// ゲームウィンドウクラス
+	KdWindow		m_window;
+
+	// FPSコントローラー
+	KdFPSController	m_fpsController;
+
+	// ゲーム終了フラグ trueで終了する
+	bool		m_endFlag = false;
+
+	//カメラ
+	std::shared_ptr<KdCamera> m_spCamera = nullptr;//スマートポインタ
+	//ポリゴン
+	std::shared_ptr<KdSquarePolygon> m_spPoly = nullptr;
+
+	// 地形データ
+	std::shared_ptr<KdModelData> m_spModel = nullptr;
+
+
+	//変数
+	Math::Vector3 m_pos;
+//=====================================================
+// シングルトンパターン
+//=====================================================
+private:
+	// 
+	Application() {}
+
+public:
+	static Application &Instance(){
+		static Application Instance;
+		return Instance;
+	}
+};
